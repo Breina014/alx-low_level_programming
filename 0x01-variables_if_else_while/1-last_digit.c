bf8444d342c504cfb2a623ed84e6a4dad6cd0612
Breina014 <breinaj2000@gmail.com>
2022-10-19 07:21:54 -0700

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,20 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
-
 /**
- * main - prints all combinations of single digits
- * Return: Always 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-  int n;
+int n;
+int m;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+ /* your code goes here */
+m = n % 10;
+if (m > 5)
 
-  for (n = 48; n < 58; n++)
-    {
-      putchar(n);
-      if (n != 57)
-	{
-	  putchar(',');
-	  putchar(' ');
-	}
-      putchar('\n')
-	return (0);
+printf("Last digit of %d is %d and is greater than $\n", n, m);
+if (m == 0)
+printf("Last digit of %d is %d and is 0\n", n, m);
+if (m < 6 && m != 0)
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
+return (0);
+}
